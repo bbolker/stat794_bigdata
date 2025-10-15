ce3b9b5bccee3ba70b2b3433bd81f95d1fe56093
Ben Bolker <bbolker@gmail.com>
2025-10-15 14:16:15 -0400

    bugfix/gillespieSSA

--- gillespie_SIR.R
+++ gillespie_SIR.R
@@ -1,10 +1,16 @@
-gsir <- function(params = list(beta = 2, gamma = 1),
+## Doob-Gillespie algorithm implementation for SIR model 
+## https://en.wikipedia.org/wiki/Gillespie_algorithm
+## https://cran.r-project.org/web/packages/GillespieSSA/index.html
+
+library(GillespieSSA)
+
+gsir <- function(parms = list(beta = 2, gamma = 1),
                  N = 10000,
                  I0 = 0.01,
                  dt = 0.2,
                  tmax = 100,
                  maxit = 1e6) {
-  I0_val <- min(1, round(N*I0))
+  I0_val <- max(1, round(N*I0))
   S <- N-I0_val
   I <- I0_val
   R <- 0
@@ -13,7 +19,7 @@ gsir <- function(params = list(beta = 2, gamma = 1),
   it <- 1
   next_t <- dt
   while (t < tmax && it < maxit && I > 0) {
-    rates <- with(params, c(infection = beta*S*I/N, recovery = gamma*I))
+    rates <- with(parms, c(infection = beta*S*I/N, recovery = gamma*I))
     nrates <- length(rates)
     event <- sample(1:nrates, prob = rates/sum(rates), size = 1)
     if (event == 1) {
@@ -35,8 +41,34 @@ gsir <- function(params = list(beta = 2, gamma = 1),
   
 
 set.seed(101)
-debug(gsir)
 res <- gsir()
 
 par(las=1)
 matplot(res$t, res[,-1], log = "y", type = "l")
+
+gSSA_sir <- function(parms = list(beta = 2, gamma = 1, N=10000),
+                     I0 = 0.01,
+                     dt = 0.2,
+                     tmax = 100,
+                     maxit = 1e6,
+                     method = ssa.d()) {
+  I0_val <- max(1, round(parms[["N"]]*I0))
+  x0 <- c(S=parms[["N"]]-I0_val, I = I0_val, R = 0)
+  nu <- matrix(c(-1,0,1,-1,0,1), nrow=3, byrow=TRUE)
+  a  <- c("beta*S*I/N", "gamma*I")
+  out <- ssa(
+    x0 = x0,
+    a = a,
+    nu = nu,
+    parms = parms,
+    tf = tmax,
+    censusInterval = dt,
+    method = method,
+    simName = "gillespieSSA_SIR",
+    verbose = FALSE)
+  out
+} 
+
+set.seed(101)
+res2 <- gSSA_sir()
+matplot(res2$data[,1], res2$data[,-1],  log = "y", type = "l")
